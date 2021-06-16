e880ccda7b479721f448b0eb008ba28dfb290e9f
SilverFlame83 <imren_83@abv.bg>
2021-06-16 19:29:58 +0300

    Updated comments

--- services/product.js
+++ services/product.js
@@ -25,11 +25,14 @@ async function getAll(query) {
 
 async function getById(id) {
     const cube = await Cube.findById(id)
-    .populate('comments')
-    .populate('accessories')
-    .populate('author')
-    .lean()
-    
+        .populate({
+            path: 'comments',
+            populate: { path: 'author' }
+        })
+        .populate('accessories')
+        .populate('author')
+        .lean()
+
     if (cube) {
         const viewModel = {
             _id: cube._id,
@@ -37,7 +40,7 @@ async function getById(id) {
             description: cube.description,
             imageUrl: cube.imageUrl,
             difficulty: cube.difficulty,
-            comments: cube.comments,
+            comments: cube.comments.map(c => ({ content: c.content, author: c.author.username })),
             accessories: cube.accessories,
             author: cube.author && cube.author.username,
             authorId: cube.author && cube.author._id
